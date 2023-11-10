1b3dbe01d885827abd06e70288a5561bf987aa6b
wangbaolong <2423024015@qq.com>
2023-11-10 18:18:32 +0800

    新增测试2

--- app/src/main/java/com/wbl/composelibrary/MainActivity.java
+++ app/src/main/java/com/wbl/composelibrary/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.wbl.composelearning.LearningTest;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
